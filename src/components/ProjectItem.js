35323a21b6f7a97aa90fdfa755359aa5e39b6d53
mark mwendwa <mwendwamark310@gmail.com>
2023-04-18 23:22:13 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies }) 
+// the ProjectItem is taking three props as input
+{
   return (
     <div className="project-item">
+      {/* h3 displays the name prop */}
       <h3>{name}</h3>
+      {/* p tag displays the about prop */}
       <p>{about}</p>
+      {/* div element with a class name of "technologies" */}
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech, index) => (
+          <span key={index}>{tech}</span>
+        ))}
       </div>
     </div>
   );
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Mark",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
